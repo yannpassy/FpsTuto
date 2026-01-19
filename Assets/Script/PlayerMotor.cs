9b3db1a0b58a710fc00db9ef7562c06d12348cdc
yannpassy <yannpassy1@gmail.com>
2026-01-19 22:35:09 +0100

    PlayerMotor
    1(11:00)

--- Assets/Script/PlayerMotor.cs
+++ Assets/Script/PlayerMotor.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    [SerializeField]private float speed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // receive input from InputManager.cs and apply it to the CharacterManager
+    public void ProcessMove(Vector2 _input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = _input.x;
+        moveDirection.z = _input.y;
+        // TransformDirection permet d'avancer vers là où le joueur regarde
+        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
     }
 }
